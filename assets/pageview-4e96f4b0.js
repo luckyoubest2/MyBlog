0af5aeee43f5e7a959354c0d7cff1812e3546b7e
luckyoubest2 <88052060+luckyoubest2@users.noreply.github.com>
2023-05-30 01:13:41 +0000

    Deploying to gh-pages from @ luckyoubest2/luckyoubest2.github.io@93c785c9ea3b6412067355746a28f1829e664b09 🚀

--- assets/pageview-4e96f4b0.js
+++
@@ -1,1 +0,0 @@
-import{a5 as e}from"./app-36504f1a.js";const p=()=>e({serverURL:"https://waline.newzone.top"});export{p as updatePageview};
---
+++ assets/pageview-ecb5fad5.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./app-c9d21dde.js";const p=()=>e({serverURL:"https://waline-ebon-gamma.vercel.app/"});export{p as updatePageview};
